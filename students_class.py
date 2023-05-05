ce96017eb1ee73e155790d458f60b88b11a5cb2d
dmhunt6373 <130405175+dmhunt6373@users.noreply.github.com>
2023-05-05 13:37:27 -0400

    Update students_class.py

--- students_class.py
+++ students_class.py
@@ -2,7 +2,8 @@
 #/var/www/virtual_webapps/alma_sis/bin/python
 #/opt/rh/rh-python34/root/usr/bin/python
 import psycopg, datetime, sys, smtplib, json
-import connect2db
+#import connect2db
+import patron_class
 
 class Students():
     con = None
@@ -32,7 +33,7 @@ Subject: pc255 from py!
         dbusername = self.config['patrons']['user']
         dbpassword = self.config['patrons']['password']
 
-        self.con = connect2db(dbhostname, dbportnumber, dbname, dbusername, dbpassword)
+        self.con = self.connect2db(self, dbhostname, dbportnumber, dbname, dbusername, dbpassword)
 
         self.getAllPatrons()
 
